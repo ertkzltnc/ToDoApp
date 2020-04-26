c9ebf83160f6ffefe1eb9018ecbedab683157960
ertkzltnc <ertkzltnc@gmail.com>
2020-04-26 15:49:47 +0300

    Startup settings (ConnectionString)

--- ToDoApp/ToDoApp.API/Startup.cs
+++ ToDoApp/ToDoApp.API/Startup.cs
@@ -6,10 +6,14 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ToDoApp.Core.UnitOfWorks;
+using ToDoApp.Data;
+using ToDoApp.Data.UnitOfWorks;
 
 namespace ToDoApp.API
 {
@@ -25,6 +29,11 @@ namespace ToDoApp.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options => {
+                options.UseSqlServer(Configuration["ConnectionStrings:SqlConstr"].ToString());
+            });
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();            
             services.AddControllers();
         }
 
