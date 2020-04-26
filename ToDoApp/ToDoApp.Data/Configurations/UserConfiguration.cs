83e4db92e66ae1b4436b90da647dcc57a905d778
ertkzltnc <ertkzltnc@gmail.com>
2020-04-26 15:23:23 +0300

    Data->Repository

--- ToDoApp/ToDoApp.Data/Configurations/UserConfiguration.cs
+++ ToDoApp/ToDoApp.Data/Configurations/UserConfiguration.cs
@@ -13,7 +13,7 @@ namespace ToDoApp.Data.Configurations
         {
             builder.HasKey(x => x.Id);
             builder.Property(x => x.Id).UseIdentityColumn();
-            builder.Property(x => x.UserName).IsRequired().HasMaxLength(50);
+            builder.Property(x => x.UserName).IsRequired().HasMaxLength(50).IsUnicode();
             builder.Property(x => x.Password).IsRequired().HasMaxLength(50);
             builder.Property(x => x.Name).IsRequired().HasMaxLength(50);
             builder.Property(x => x.LastName).IsRequired().HasMaxLength(50);
---
+++ ToDoApp/ToDoApp.Data/Repositories/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.Core.Models;
+using ToDoApp.Core.Repositories;
+
+namespace ToDoApp.Data.Repositories
+{
+    public class UserRepository : Repository<User>, IUserRepository
+    {
+        private AppDbContext appDbContext { get => _context as AppDbContext; }
+        public UserRepository(DbContext context) : base(context)
+        {
+        }
+
+        public async Task<User> Login(string userName, string password)
+        {
+            return await appDbContext.Users.SingleOrDefaultAsync
+                (x => x.UserName == userName && x.Password == password);
+        }
+    }
+}
